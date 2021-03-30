37450425f7951c948c6aebd25395fa55e9ee4f45
Roman Viryasov <roman.viryasov@citeck.ru>
2021-03-31 01:06:25 +0400

    Add Tasks

---
+++ src/Task5.java
@@ -0,0 +1,13 @@
+public class Task5 {
+
+    public static void main(String[] args) {
+
+//        Что будет выведено в результате выполнения программы?
+
+        int x = 0, y = 0;
+
+        System.out.println(x++==++x);
+        System.out.println(++y==y++);
+
+    }
+}
