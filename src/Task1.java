ae93816ab199de8541e6c2f48e7ccefff1a626bc
Roman Viryasov <roman.viryasov@citeck.ru>
2021-03-30 23:15:06 +0400

    Add Task1

---
+++ src/Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("test" == "test");
+        System.out.println("test" == new String("test"));
+        System.out.println(new String("test") == new String("test"));
+    }
+}
